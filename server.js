fce6a61adf7ed2e020e34e8894055bcced0d53cd
RajatSharma07 <rajat.sharma@xebia.com>
2024-07-18 23:49:09 +0530

    initial commit with kafka, admin in kafka and topic in kafka

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { kafka } = require('./kafka');
+require('dotenv');
+const app = express();
+app.listen(process.env.PORT || 5500, () => {
+    console.log(`App is listening on ${process.env.PORT || 5500}`);
+});
+
+async function init(){
+    const admin = kafka.admin();
+    console.log('Admin Connecting');
+    admin.connect();
+    console.log('Admin Connected');
+    console.log('Creating topic [send email]');
+    // await admin.createTopics({
+    //     topics: [
+    //         {
+    //             topic: "send email",
+    //             numPartitions: 2,
+    //         }
+    //     ]
+    // });
+    await admin.createTopics({
+        topics: [
+            {
+                topic: 'send_email',
+            }
+        ]
+    })
+    console.log('topic created [send email]');
+    console.log('Disconnecting Admin');
+    admin.disconnect();
+}
+init();
